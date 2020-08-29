eff71755aaa3d8e5a58f5f091bbcf3c59f8c5676
Yesenia Ibarra <yessyibarra17@gmail.com>
2020-08-28 17:30:20 -0700

    Updated margin on main header and login card. Added a background color , need to figure out routes now.

--- apollo/routes/html-routes.js
+++ apollo/routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.render("signup")
+    res.render("signup");
   });
 
   app.get("/login", (req, res) => {
